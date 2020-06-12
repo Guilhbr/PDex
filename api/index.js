1ff6fb52247584eebb1c37b36d24a18c3299f7ca
Guilhbr <guilherme.quadros.souza@gmail.com>
2020-06-12 09:45:59 +0800

    Refactor

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const axios = require('axios')
 const NodeCache = require( "node-cache" );
+
+// Cache for pokemon entries, resets when server is relaunched
 const cache = new NodeCache();
 
 // Find first english entry and replace \n and \f characters with spaces
-function findAndSanitizeEntry(entries) {
+function findAndSanitizeEntry(entries = []) {
   const entry = entries.find(e => e.language.name === 'en')
-  return flavor = entry['flavor_text'].replace(/\n|\f/g, ' ')
+  return flavor = entry && entry['flavor_text'].replace(/\n|\f/g, ' ')
 }
 
 // Check if pokemon is cached, if not call the pokemon api to find its description
@@ -18,7 +20,7 @@ function getPokemonDesc (name) {
     .then(response => {
       const description = findAndSanitizeEntry(response.data.flavor_text_entries)
       const pokemon = {name: response.data.name, description}
-      cache.set(name, pokemon, 86400) //24 hours
+      cache.set(name, pokemon)
       return pokemon
     })
 }
@@ -35,16 +37,16 @@ function getTranslation (desc) {
 exports.getPokemon = async (req, res, next) => {
   const name = req.params.name.toLowerCase()
   try {
-    let pokemon = await getPokemonDesc(name, next)
+    let pokemon = await getPokemonDesc(name)
     try {
-      pokemon.description = await getTranslation(pokemon.description, next)
+      pokemon.description = await getTranslation(pokemon.description)
       res.send(pokemon)
     } catch(error) { 
-      res.status(error.response.status).send('Error translating description.') 
+      res.status(error.response.status || 500).send('Error translating description.') 
       next(error)
     }
   } catch (error) { 
-    res.status(error.response.status).send('Error finding pokemon.') 
+    res.status(error.response.status || 500).send('Error finding pokemon.') 
     next(error)
   }
 }
